b3a2717bb016441cad0ec0328ae7a14f591c1ff6
KJ.O <nomeansno@Live.ca>
2022-05-05 13:28:51 +0900

    Add files via upload

---
+++ save.py
@@ -0,0 +1,9 @@
+import csv
+
+def save_to_file(jobs,name):
+  file=open(f"{name}.csv",mode="w")
+  writer=csv.writer(file)
+  writer.writerow(["title","company","Apply_link"])
+  for job in jobs:
+    writer.writerow(list(job.values())[0:3])
+  return
